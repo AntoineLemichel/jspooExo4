45ce0b7166895eec006321d4b40df02db161e4ec
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-11-13 09:35:38 +0100

    Adding file JSON with data into. Add table into index.html and script JS with AJAX

--- js/main.js
+++ js/main.js
@@ -1,43 +1,132 @@
-function ajaxGet(url, callback) {
-  var req = new XMLHttpRequest();
-  req.open("GET", url);
-  req.addEventListener("load", function () {
-    if (req.status >= 200 && req.status < 400) {
-      callback(req.responseText);
-    } else {
-      console.error(req.status + " " + req.statusText + " " + url);
-    }
-  });
-  req.addEventListener("error", function () {
-    console.error("Erreur réseau avec l'URL " + url);
-  });
-  req.send(null);
+// function ajaxGet(url, callback) {
+//   var req = new XMLHttpRequest();
+//   req.open("GET", url);
+//   req.addEventListener("load", function () {
+//     if (req.status >= 200 && req.status < 400) {
+//       callback(req.responseText);
+//     } else {
+//       console.error(req.status + " " + req.statusText + " " + url);
+//     }
+//   });
+//   req.addEventListener("error", function () {
+//     console.error("Erreur réseau avec l'URL " + url);
+//   });
+//   req.send(null);
+// }
+
+
+
+// ajaxGet("http://localhost:8080/data.json", function (reponse) {
+//   var user = JSON.parse(reponse);
+//   user.users.forEach(function (users) {
+
+
+//     parentManager = document.getElementById('table');
+//     tr = document.createElement('tr');
+//     nameElement = document.createElement('td');
+//     nameElement.innerHTML = users.name;
+
+
+//     idElement = document.createElement('td');
+//     idElement.innerHTML = users.id;
+
+//     ageElement = document.createElement('td');
+//     ageElement.innerHTML = users.age;
+
+//     cityElement = document.createElement('td');
+//     cityElement.innerHTML = users.city;
+
+//     parentManager.appendChild(tr);
+//     tr.appendChild(idElement);
+//     tr.appendChild(nameElement);
+//     tr.appendChild(ageElement);
+//     tr.appendChild(cityElement);
+
+//     document.getElementById('select').addEventListener('change', function (e) {
+
+//     });
+
+//   });
+// });
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+
+    let myDataObject = JSON.parse(this.responseText);
+    myDataObject.users.forEach(function (users) {
+      dataJson.getInfos(users);
+      function setCompare(){
+        users.reverse();
+      }
+      document.getElementById('select').addEventListener("change", function (e) {
+        if (e.target.value == "ID") {
+          setCompare(users.id, users.id);
+        } else if (e.target.value == "Name") {
+
+        } else if (e.target.value == "Age") {
+
+        } else if (e.target.value == "Ctiy") {
+
+        }
+      });
+    });
+  }
+}
+
+xhttp.open("GET", "data.json", true);
+xhttp.send();
+
+let dataJson = {
+  id: "",
+  name: "",
+  age: "",
+  city: "",
+
+  getInfos: function (object) {
+
+    this.id = object.id;
+    this.name = object.name;
+    this.age = object.age;
+    this.city = object.city;
+
+    htmlManager.setDocument(this.id, this.name, this.age, this.city);
+
+  }
 }
 
 
+let htmlManager = {
+  setCompare: function (a, b) {
+    console.log("test");
+    return Number(a) - Number(b);
+  },
 
-ajaxGet("http://localhost:8080/data.json", function (reponse) {
-  var user = JSON.parse(reponse);
-  user.users.forEach(function (users) {
-    parentManager = document.getElementById('table');
+  setDocument: function (id, name, age, city) {
+
+    parentManger = document.getElementById("table");
     tr = document.createElement('tr');
-    nameElement = document.createElement('td');
-    nameElement.innerHTML = users.name;
-
 
     idElement = document.createElement('td');
-    idElement.innerHTML = users.id;
+    idElement.innerHTML = id;
+
+    nameElement = document.createElement('td');
+    nameElement.innerHTML = name;
 
     ageElement = document.createElement('td');
-    ageElement.innerHTML = users.age;
+    ageElement.innerHTML = age;
 
     cityElement = document.createElement('td');
-    cityElement.innerHTML = users.city;
+    cityElement.innerHTML = city;
 
-    parentManager.appendChild(tr);
+
+
+    parentManger.appendChild(tr);
     tr.appendChild(idElement);
     tr.appendChild(nameElement);
     tr.appendChild(ageElement);
     tr.appendChild(cityElement);
-  });
-});
+
+  }
+}
